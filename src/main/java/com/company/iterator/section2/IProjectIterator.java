b4e19e27345bb60acd9dd63043f47bbc1ccc0cb7
ossaw <jianghao7625@gmail.com>
2018-10-10 13:50:21 +0800

    upload project

---
+++ src/main/java/com/company/iterator/section2/IProjectIterator.java
@@ -0,0 +1,12 @@
+package com.company.iterator.section2;
+
+import java.util.Iterator;
+
+/**
+ * @author cbf4Life cbf4life@126.com
+ * I'm glad to share my knowledge with you all.
+ * 定义个Iterator接口
+ */
+public interface IProjectIterator<E> extends Iterator<E> {
+
+}
